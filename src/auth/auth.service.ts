55e50ed71dc38d11036f116d8797740d59cf7a70
Maria <mlatysheva@gmail.com>
2025-02-17 21:48:46 +0100

    feat: implemented sign JWT strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,39 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import ms from 'ms';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './token-payload.interface';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+
+    expires.setMilliseconds(expires.getMilliseconds() + ms('10h'));
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      secure: true,
+      httpOnly: true,
+      expires,
+    });
+
+    return { tokenPayload };
+  }
 
   async verifyUser(email: string, password: string) {
     try {
