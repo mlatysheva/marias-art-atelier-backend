874529b8c82aeac16dcaaa465e7d5007a4356d05
Maria <mlatysheva@gmail.com>
2025-02-24 22:13:37 +0100

    feat: added JWT strategy and cookie-authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -10,6 +10,7 @@ import { AuthModule } from './auth/auth.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
         const isProduction = configService.get('NODE_ENV') === 'production';
+
         return {
           pinoHttp: {
             transport: isProduction
@@ -26,8 +27,8 @@ import { AuthModule } from './auth/auth.module';
       },
       inject: [ConfigService],
     }),
-    UsersModule,
     ConfigModule.forRoot(),
+    UsersModule,
     AuthModule,
   ],
   controllers: [],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/token-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -9,7 +19,13 @@ export class UsersController {
 
   @Post()
   @UseInterceptors(NoFilesInterceptor())
-  createUser(@Body() createUserRequest: CreateUserRequest) {
-    return this.usersService.createUser(createUserRequest);
+  createUser(@Body() request: CreateUserRequest) {
+    return this.usersService.createUser(request);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
